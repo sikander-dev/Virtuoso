e5a7fe7fca5e1af9ea1a008f4e95bec64dd69bb4
sikander mahan <sikander@riva.co>
2015-10-23 13:34:57 +0530

    Initial commit

---
+++ Virtuoso/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Virtuoso
+//
+//  Created by sikander.m on 10/23/15.
+//  Copyright (c) 2015 sikander.m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ VirtuosoTests/VirtuosoTests.m
@@ -0,0 +1,40 @@
+//
+//  VirtuosoTests.m
+//  VirtuosoTests
+//
+//  Created by sikander.m on 10/23/15.
+//  Copyright (c) 2015 sikander.m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface VirtuosoTests : XCTestCase
+
+@end
+
+@implementation VirtuosoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
